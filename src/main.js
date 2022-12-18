87c3dd177b6fd3ea0550d8d86a5c11a32b45020c
root <root@DESKTOP-D8MJ84U>
2022-12-18 22:25:46 +0100

    added all

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const child_process = require("child_process");
 
 const argv = process.argv.slice(2);
 const MqttConnectionString = argv[0] || "mqtt://127.0.0.1:1883";
-	console.log ("using MQTT-Server"  + MqttConnectionString);
+console.log("using MQTT-Server" + MqttConnectionString);
 const client = mqtt.connect(MqttConnectionString, {
   clientId: "robot",
   client: "ROBOT",
@@ -18,24 +18,27 @@ client.on("connect", function () {
 console.log(`Publishing test message to ${topic}`);
 client.publish(topic, "Test message");
 
-const child = child_process.spawn("xboxdrv",["--detach-kernel-driver"]).on("error", (err) => {
-  console.error(`Run into error: ${err}`);
-});
+const child = child_process
+  .spawn("xboxdrv", ["--detach-kernel-driver"])
+  .on("error", (err) => {
+    console.error(`Run into error: ${err}`);
+  });
 let lastData = undefined;
 child.stdout.on("data", function (data) {
   //Here is where the output goes
 
- //console.log("stdout: " + data);
+  //console.log("stdout: " + data);
 
   data = data.toString();
   if (data !== lastData) {
     console.log("stdout: " + data);
-    const regex = /X1\:[ ]*([-\d]+)[ ]*Y1\:[ ]*([-\d]+)[ ]*X2\:[ ]*([-\d]+)[ ]*Y2\:[ ]*([-\d]+)/ig;
+    const regex =
+      /X1\:[ ]*([-\d]+)[ ]*Y1\:[ ]*([-\d]+)[ ]*X2\:[ ]*([-\d]+)[ ]*Y2\:[ ]*([-\d]+)/gi;
     const matches = regex.exec(data);
     if (!matches) {
-    console.log("no match for " +data);
-    return;
-  } 
+      console.log("no match for " + data);
+      return;
+    }
     const msg = `${matches[1]}:${matches[2]}:${matches[3]}:${matches[4]}`;
     client.publish(topic, msg);
   }
